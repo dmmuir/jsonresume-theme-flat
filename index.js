9e7dfe59d1cba91b5f8ad4e6de8c505422938ad7
dm065392 <Dan.Muir@cerner.com>
2021-04-19 14:30:10 -0400

    Modify for dmmuir resume

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var fs = require("fs");
 var Handlebars = require("handlebars");
 
-module.exports = {
-	render: render
-};
-
 function render(resume) {
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
@@ -17,3 +13,16 @@ function render(resume) {
 Handlebars.registerHelper("nl2br", function(value) {
 	return (value || "").replace(/\n/g, "</p><p>");
 });
+
+const readFrom = async (stream) => {
+    const chunks = [];
+    for await (const chunk of stream) chunks.push(chunk);
+    return JSON.parse(Buffer.concat(chunks).toString("utf8"));
+};
+
+readFrom(process.stdin)
+.then(resume => {
+	console.log(render(resume));
+}).catch((err) => {
+        console.error(err);
+    });;
